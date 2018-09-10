6b174bbd58da0690e771de41e11a7d0dc7944157
Darren Ladner <dxladner@gmail.com>
2018-09-09 20:22:10 -0500

    first commit

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+    @section('title')
+        <title>Laravel Test Page</title>
+    @endsection
+    @section('content')
+        <h1>Laravel Test Page</h1>
+        @if(count($sports) > 0)
+            @foreach($sports as $sport)
+                {{ $sport }} <br>
+            @endforeach
+        @else
+            <h1> Sorry, nothing to show...</h1>
+        @endif
+    @endsection
